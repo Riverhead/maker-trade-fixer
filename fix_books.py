bce89e97c7452f2ea16834c72610c59267143ebd
Riverhead <james@makerdao.com>
2017-02-02 09:26:11 -0500

    One scanrio working - needs pass/fail checking

--- fix_books.py
+++ fix_books.py
@@ -3,34 +3,38 @@
 import json
 from web3 import Web3, RPCProvider
 from operator import itemgetter
+import time
 
 precision = 1000000000000000000
 
 mkr_addr = "0xc66ea802717bfb9833400264dd12c2bceaa34a6d"
-weth_addr = "0xC66eA802717bFb9833400264Dd12c2bCeAa34a6d"
+weth_addr = "0xecf8f87f810ecf450940c9f60066b4a7a501d6a7" 
 geth_addr = "0xa74476443119A942dE498590Fe1f2454d7D4aC0d"
-
+market_addr = "0xa1B5eEdc73a978d181d1eA322ba20f0474Bb2A25"
 web3rpc = Web3(RPCProvider())
 
-web3rpc.eth.defaultAccount = "0x6E39564ecFD4B5b0bA36CD944a46bCA6063cACE5"
+acct_owner = "0x6E39564ecFD4B5b0bA36CD944a46bCA6063cACE5"
+
+web3rpc.eth.defaultAccount = acct_owner
 web3rpc.eth.defaultBlock = "latest"
 # Can also be an integer or one of "latest", "pending", "earliest"
 
 with open('market.abi', 'r') as abi_file:
   abi_json = abi_file.read().replace('\n','')
 abi = json.loads(abi_json)
-market_contract = web3rpc.eth.contract(abi, address="0xa1B5eEdc73a978d181d1eA322ba20f0474Bb2A25")
+market_contract = web3rpc.eth.contract(abi, address=market_addr)
+
 
 with open('erc20.abi', 'r') as abi_file:
   abi_json = abi_file.read().replace('\n','')
 abi = json.loads(abi_json)
-weth_contract = web3rpc.eth.contract(abi, address="0xECF8F87f810EcF450940c9f60066b4a7a501d6A7")
-mkr_contract = web3rpc.eth.contract(abi, address="0xC66eA802717bFb9833400264Dd12c2bCeAa34a6d")
+weth_contract = web3rpc.eth.contract(abi, address=weth_addr)
+mkr_contract = web3rpc.eth.contract(abi, address=mkr_addr)
 
-weth_balance = float(weth_contract.call().balanceOf("0x6E39564ecFD4B5b0bA36CD944a46bCA6063cACE5"))/precision
-mkr_balance  = float(mkr_contract.call().balanceOf("0x6E39564ecFD4B5b0bA36CD944a46bCA6063cACE5"))/precision
+weth_balance = float(weth_contract.call().balanceOf(acct_owner))/precision
+mkr_balance  = float(mkr_contract.call().balanceOf(acct_owner))/precision
 
-print("\nBalance available to the fixer:                                                   %0.5f ETH - %0.5f MKR\n" % (weth_balance, mkr_balance))
+print("\nBalances: %0.5f WETH - %0.5f MKR\n" % (weth_balance, mkr_balance))
 
 last_offer_id = market_contract.call().last_offer_id()
 
@@ -40,6 +44,9 @@ offers = []
 while id <  last_offer_id + 1:
   offers.append(market_contract.call().offers(id))
   id = id + 1
+
+
+print("There are %i offers" % last_offer_id)
 
 id=0
 
@@ -55,45 +62,47 @@ for offer in offers:
     buy_which_token = offer[3]
     owner = offer[4][2:8]
 
-    if sell_which_token == mkr_addr:
-      s_token = "MKR"
-    elif sell_which_token == geth_addr:
-      s_token = "W-GNT"
-    else:
-      s_token = "W-ETH"
 
-    if buy_which_token == mkr_addr:
-      b_token = "MKR"
-    elif buy_which_token == geth_addr:
-      b_token = "W-GNT"
-    else:
-      b_token = "W-ETH"
-
-    if s_token == "MKR" and b_token == "W-ETH":
+    if sell_which_token == mkr_addr and buy_which_token == weth_addr:
       sell_orders.append([id, sell_how_much, buy_how_much/sell_how_much, buy_how_much, owner])
 
-    if s_token == "W-ETH" and b_token == "MKR":
+    if sell_which_token == weth_addr and buy_which_token == mkr_addr:
       buy_orders.append([id, buy_how_much, sell_how_much/buy_how_much, buy_how_much, owner])
-    id = id + 1
+  id = id + 1
 
 buy_orders.sort(key=itemgetter(2), reverse=True)
-bid_id = float(buy_orders[0][0])
+print("%s" % buy_orders[0])
+bid_id = int(buy_orders[0][0])
 bid    = float(buy_orders[0][2])
 bid_qty     = float(buy_orders[0][1]) 
 print ("Highest bid is for %0.5f MKR @ %0.5f ETH/MKR" % (bid_qty,bid))
 
 sell_orders.sort(key=itemgetter(2), reverse=False)
-ask_id = float(buy_orders[0][0])
+ask_id = int(sell_orders[0][0])
 ask = float(sell_orders[0][2])
 ask_qty  = float(sell_orders[0][1]) 
 print ("Lowest ask is for %0.5f MKR @ %0.5f ETH/MKR" % (ask_qty,ask))
 
-result = market_contract.call().buy(int(bid_id), web3rpc.toWei(0.01, 'Ether'))
+if float(weth_contract.call().allowance(acct_owner, market_addr)) < 0.1:
+  result = weth_contract.call().approve(acct_owner, int(0.1*precision))
+  #print ("Update allowance: %s" % result)
+  while weth_contract.call().allowance(weth_addr, market_addr) < 0.1:
+    print("Waiting for allowance to be applied")
+    time.sleep(3)
+print("WETH Allowance: %f" % (weth_contract.call().allowance(acct_owner, market_addr)/precision))
 
-if bid >= ask:
+if float(mkr_contract.call().allowance(acct_owner, market_addr)) < 0.1:
+  result = mkr_contract.call().approve(acct_owner, int(0.1*precision))
+  #print ("Update allowance: %s" % result)
+  while mkr_contract.call().allowance(mkr_addr, market_addr) < 0.1:
+    print("Waiting for allowance to be applied")
+    time.sleep(3)
+print("MKR Allowance: %f" % (mkr_contract.call().allowance(acct_owner, market_addr)/precision))
+
+if round(bid,5) >= round(ask,5):
   print("\nAction needed!")
   if bid_qty > ask_qty:
-    if weth_balance < 1:
+    if weth_balance < 0.2:
       print ("Not enough ETH!")
     else:
       print ("Buy from Ask book and sell to Bid book")
@@ -103,11 +112,19 @@ if bid >= ask:
       print ("Not enough MKR!")
     else:
       print ("Sell from Bid book and buy Ask book")
-      if mkr_balance < bq:
+      if mkr_balance > bid_qty:
         bid_id = int(bid_id)
-        quantity = int(0.2*precision)
-        print("Sell: [%i] %i MKR" % (bid_id, quantity))
-        result = market_contract.call().buy(bid_id, quantity)
-        print("%s" % result)
+        try:
+          if market_contract.call().buy(bid_id, int(bid_qty*bid*precision)) and market_contract.call().buy(ask_id, int(bid_qty/bid*precision)):
+            try: 
+              result = market_contract.transact().buy(bid_id, int(bid_qty*bid*precision))
+              print("%s" % result)
+              result = market_contract.transact().buy(ask_id, int(bid_qty*precision))
+              print("%s" % result)
+            except:
+              print("Transaction timed out.") 
+        except:
+          print("Sell: [%i] %f ETH [XXX]" % (ask_id, bid_qty*ask))
+          print("Sell: [%i] %f ETH [XXX]" % (bid_id, bid_qty))
 else:
  print ("All is well")
