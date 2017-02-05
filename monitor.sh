e8d0f8c98113d557d6cde2ac049c24801ee5d7a4
Riverhead <james@makerdao.com>
2017-02-05 11:27:04 -0500

    Change output to json, bug fixes

--- monitor.sh
+++ monitor.sh
@@ -2,6 +2,5 @@ while [ true ] ; do
   ./fix_books.py
   sed -i '$s/,$//' maker-matcher.json
   #sed -i '$s/"}{"/"},{"/' maker-matcher.json
-  scp maker-matcher.json reidy@riverhead.org:www/.
   sleep 5
 done
